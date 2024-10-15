efe980e74002c6324283e77e35863cba2cd60dc0
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-10-15 16:30:16 +0900

    Feat: example of retrieving search terms

--- app/pages/home/clubRanking.tsx
+++ app/pages/home/clubRanking.tsx
@@ -1,4 +1,13 @@
+"use client";
+
+import { useContext } from "react";
+import SearchTermContext from "@/context/searchTermContext";
+
 const ClubRanking = () => {
+  const { searchTerm } = useContext(SearchTermContext);
+
+  console.log("context에 저장된 검색어 :", searchTerm);
+
   return (
     <section className="py-10">
       <h2>동아리 랭킹</h2>
