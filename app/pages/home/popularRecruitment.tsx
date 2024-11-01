8b739816b712ff9c55089a902021545091007f90
inf0craw1 <inf0craw1@naver.com>
2024-11-01 19:35:47 +0900

    feat: Add popular recruitments section

--- app/pages/home/popularRecruitment.tsx
+++ app/pages/home/popularRecruitment.tsx
@@ -1,7 +1,19 @@
+import RecruitmentItem from "@/components/recruitmentItem";
+
 const PopularRecruitment = () => {
   return (
-    <section className="py-10">
-      <h2>인기 모집공고</h2>
+    <section className="mx-auto mt-[60px] bg-background">
+      <h2 className="text-h1_contents_title">인기 모집공고</h2>
+      <div className="grid grid-cols-4 gap-x-[16px] gap-y-[28px] py-[30px]">
+        <RecruitmentItem></RecruitmentItem>
+        <RecruitmentItem></RecruitmentItem>
+        <RecruitmentItem></RecruitmentItem>
+        <RecruitmentItem></RecruitmentItem>
+        <RecruitmentItem></RecruitmentItem>
+        <RecruitmentItem></RecruitmentItem>
+        <RecruitmentItem></RecruitmentItem>
+        <RecruitmentItem></RecruitmentItem>
+      </div>
     </section>
   );
 };
