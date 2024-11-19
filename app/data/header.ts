3b0923a2303efe39d4b9eeef53d1455ec423f991
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-11-19 10:19:13 +0900

    update: header.ts

--- app/data/header.ts
+++ app/data/header.ts
@@ -1,8 +1,17 @@
-export const HEADER_MENU = [
+export const HEADER_MENU_DEFAULT = [
   { id: 0, title: "모집 공고", path: "/recruitment" },
   { id: 1, title: "동아리 탐색", path: "/exploration" },
   { id: 2, title: "동아리 지원", path: "/application" },
   { id: 3, title: "커뮤니티", path: "/community" },
+];
+
+export const HEADER_MENU_SM = [
+  { id: 0, title: "홈", path: "/" },
+  { id: 1, title: "모집 공고", path: "/recruitment" },
+  { id: 2, title: "동아리 탐색", path: "/exploration" },
+  { id: 3, title: "동아리 지원", path: "/application" },
+  { id: 4, title: "동아리 관리", path: "/management" },
+  { id: 5, title: "커뮤니티", path: "/community" },
 ];
 
 export const USER_MENU = [
