9d52eb2e0bc4e4d2b17121f596a57eb1fa63aef4
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-11-19 10:21:20 +0900

    style: delete auto scroll

--- app/components/layout/layout.tsx
+++ app/components/layout/layout.tsx
@@ -12,7 +12,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <SearchTermContext.Provider value={{ searchTerm, setSearchTerm }}>
       <div className="flex flex-col min-h-screen">
         <Header />
-        <main className="flex-grow overflow-y-auto flex justify-center items-center">
+        <main className="flex-grow flex justify-center items-center">
           <div className="w-full max-w-screen-sm sm:max-w-screen-md lg:max-w-screen-lg px-4 md:px-5">
             {children}
           </div>
