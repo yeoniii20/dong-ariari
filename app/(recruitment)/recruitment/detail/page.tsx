6e32269955d242f0e3dd843342a40cc84b137feb
yeoniii20 <commetoi_yeoni@naver.com>
2024-11-22 01:35:54 +0900

    feat: Add recruitment detail page

---
+++ app/(recruitment)/recruitment/detail/page.tsx
@@ -0,0 +1,13 @@
+import Keyword from "@/components/button/keyword";
+import ShareButton from "@/components/button/shareButton";
+
+const RecruitmentDetail = () => {
+  return (
+    <div className="bg-slate-200">
+      <Keyword />
+      <ShareButton />
+    </div>
+  );
+};
+
+export default RecruitmentDetail;
