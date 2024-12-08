8d67ee80eb2a252d84c034a0ea2a4ff8410c72e9
yeoniii20 <commetoi_yeoni@naver.com>
2024-12-09 00:54:47 +0900

    Design: recruitment detail page

--- app/(recruitment)/recruitment/detail/content/clubActivities.tsx
+++ app/(recruitment)/recruitment/detail/content/clubActivities.tsx
@@ -7,14 +7,17 @@ import AcceptanceReviewDropdown from "@/components/dropdown/acceptanceReviewDrop
 
 const ACCEPTANCE_REVIEWS = [
   {
+    id: 0,
     title: "2024년 봄학기 동아리 모집",
     date: "2024.12.31",
   },
   {
+    id: 1,
     title: "가을학기 동아리 모집 후기",
     date: "2024.10.15",
   },
   {
+    id: 2,
     title: "여름방학 동아리 모집 후기",
     date: "2024.07.20",
   },
@@ -22,14 +25,17 @@ const ACCEPTANCE_REVIEWS = [
 
 const RECRUITMENT_CARDS = [
   {
+    id: 0,
     title: "2024년 봄학기 동아리 모집",
     date: "2024.12.31",
   },
   {
+    id: 1,
     title: "가을학기 동아리 모집 공고",
     date: "2024.10.15",
   },
   {
+    id: 2,
     title: "여름방학 동아리 모집 안내",
     date: "2024.07.20",
   },
@@ -37,45 +43,48 @@ const RECRUITMENT_CARDS = [
 
 const ClubActivities = () => {
   return (
-    <div className="bg-sub_bg w-full justify-center">
-      <h1 className="text-mobile_h1_contents_title mb-5 pt-8 md:pt-[44px] md:text-h1_contents_title text-text1">
-        동아리 모집 안내
-      </h1>
-      <textarea className="w-full h-[247px] py-5 px-4 rounded-2xl" />
-      <h1 className="text-mobile_h1_contents_title md:text-h1_contents_title text-text1 mt-[68px]">
-        동아리 합격 후기
-      </h1>
-      <div className="flex flex-col mt-5 gap-3 md:gap-[14px]">
-        {ACCEPTANCE_REVIEWS.map((item) => {
-          return (
-            <AcceptanceReviewDropdown
-              key={item.title}
-              title={item.title}
-              date={item.date}
-              onClick={() => {}}
-            />
-          );
-        })}
-      </div>
-      <div className="flex justify-center mt-9 md:mt-10">
-        <PlusBtn title={"더보기"} onClick={() => {}} />
-      </div>
-      <h1 className="text-mobile_h1_contents_title mt-12 md:text-h1_contents_title text-text1 md:mt-[68px]">
-        이전 모집 공고
-      </h1>
-      <div className="flex flex-col mt-5 gap-3 md:gap-[14px]">
-        {RECRUITMENT_CARDS.map((item) => {
-          return (
-            <RecruitmentCard
-              title={item.title}
-              date={item.date}
-              onClick={() => {}}
-            />
-          );
-        })}
-      </div>
-      <div className="flex justify-center pb-[80px] md:pb-[124px] mt-9 md:mt-10">
-        <PlusBtn title={"더보기"} onClick={() => {}} />
+    <div className="bg-sub_bg flex justify-center items-center w-full">
+      <div className="w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-lx px-4 md:px-5">
+        <h1 className="text-mobile_h1_contents_title mb-5 pt-8 md:pt-[44px] md:text-h1_contents_title text-text1">
+          동아리 모집 안내
+        </h1>
+        <textarea className="w-full h-[247px] py-5 px-4 rounded-2xl" />
+        <h1 className="text-mobile_h1_contents_title md:text-h1_contents_title text-text1 mt-[68px]">
+          동아리 합격 후기
+        </h1>
+        <div className="flex flex-col mt-5 gap-3 md:gap-[14px]">
+          {ACCEPTANCE_REVIEWS.map((item) => {
+            return (
+              <AcceptanceReviewDropdown
+                key={item.id}
+                title={item.title}
+                date={item.date}
+                onClick={() => {}}
+              />
+            );
+          })}
+        </div>
+        <div className="flex justify-center mt-9 md:mt-10">
+          <PlusBtn title={"더보기"} onClick={() => {}} />
+        </div>
+        <h1 className="text-mobile_h1_contents_title mt-12 md:text-h1_contents_title text-text1 md:mt-[68px]">
+          이전 모집 공고
+        </h1>
+        <div className="flex flex-col mt-5 gap-3 md:gap-[14px]">
+          {RECRUITMENT_CARDS.map((item) => {
+            return (
+              <RecruitmentCard
+                key={item.id}
+                title={item.title}
+                date={item.date}
+                onClick={() => {}}
+              />
+            );
+          })}
+        </div>
+        <div className="flex justify-center pb-[80px] md:pb-[124px] mt-9 md:mt-10">
+          <PlusBtn title={"더보기"} onClick={() => {}} />
+        </div>
       </div>
     </div>
   );
--- app/(recruitment)/recruitment/detail/content/clubInfo.tsx
+++ app/(recruitment)/recruitment/detail/content/clubInfo.tsx
@@ -10,6 +10,9 @@ import RecruitmentGuideFloatingBar from "@/components/bar/floatingBar/recruitmen
 import PointStatusFloatingBar from "@/components/bar/floatingBar/pointStatusFloatingBar";
 import MobilePointStatusFloatingBar from "@/components/bar/floatingBar/mobilePointStatusFloatingBar";
 import RecruitmentBottomBar from "@/components/bar/floatingBar/recruitmentBottomBar";
+import IconBtn from "@/components/button/withIconBtn/IconBtn";
+import RecruitmentSummary from "../components/recruitmentSummary";
+import ClubProfileCard from "@/components/card/clubProfileCard";
 
 const ClubInfo = () => {
   const [isHeart, setIsHeart] = useState<boolean>(false);
@@ -19,101 +22,130 @@ const ClubInfo = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row md:gap-14">
-      <div className="w-full max-w-[792px]">
-        <Image
-          src={test_image}
-          alt={"test_image"}
-          width={792}
-          height={792}
-          layout="responsive"
-          className="rounded-48 object-cover"
-        />
-      </div>
-      <div className="flex flex-col mt-[18px] md:pt-[6px]">
-        <div className="block md:hidden">
-          <div className="flex flex-col gap-[18px]">
-            <div className="flex justify-between items-center">
-              <div className="flex gap-3 items-center">
-                <Image
-                  src={test_image}
-                  alt={"Test Image"}
-                  width={68}
-                  height={68}
-                  className="rounded-full object-cover"
-                />
-                <div className="flex flex-col gap-2">
-                  <h1 className="text-mobile_h1_contents_title text-text1">
-                    동아리 이름
-                  </h1>
-                  <p className="text-subtext2 text-mobile_body3_m">
-                    소속 | 분야 | 지역 | 대상
-                  </p>
-                </div>
-              </div>
-              <p></p>
-              <div className="cursor-pointer" onClick={onHeartClick}>
-                {isHeart ? (
-                  <MdFavorite size={24} color="#D1F75D" />
-                ) : (
-                  <MdFavorite size={24} color="#E3E3E3" />
-                )}
-              </div>
-            </div>
-            <p className="text-mobile_body1_r text-subtext1">
-              동아리 소개 입니다.
+    <div className="flex justify-center items-center w-full">
+      <div className="flex flex-col mt-2 sm_md:flex-row sm_md:gap-[27px] md:pb-10 md:pt-8 md:flex-row md:gap-14 w-full max-w-screen-sm sm:max-w-screen-md md:max-w-screen-lg lg:max-w-screen-lx px-4 md:px-5">
+        <div className="w-full max-w-[792px]">
+          <Image
+            src={test_image}
+            alt={"test_image"}
+            width={792}
+            height={792}
+            layout="responsive"
+            className="rounded-48 object-cover"
+          />
+        </div>
+        {/* 모바일 화면 */}
+        <div>
+          <div className="flex justify-between items-start mt-[18px] md:hidden">
+            <p className="text-mobile_h1_contents_title text-text1">
+              모집공고 제목 혹은 <br />
+              동아리 한줄 소개 작성
             </p>
-          </div>
-        </div>
-        <div className="hidden md:block">
-          <div className="relative w-[94px] h-[94px]">
-            <Image
-              src={test_image}
-              alt={"Test Image"}
-              width={94}
-              height={94}
-              className="rounded-full object-cover"
+            <IconBtn
+              type={"declaration"}
+              size={"large"}
+              title={""}
+              onClick={() => {}}
             />
-            <div
-              className="absolute bottom-0 right-0 cursor-pointer"
-              onClick={onHeartClick}
-            >
-              {isHeart ? (
-                <MdFavorite size={24} color="#D1F75D" />
-              ) : (
-                <MdFavorite size={24} color="#E3E3E3" />
-              )}
-            </div>
           </div>
-          <h1 className="text-h1_contents_title mb-[15px] mt-[26px]">
-            동아리 이름
-          </h1>
-          <p className="text-body1_r">동아리 소개를 작성해주세요.</p>
-          <div className="w-[340px] mt-10">
-            <Keyword />
-          </div>
-          <div className="h-0.5 bg-menuborder mt-8 mb-7" />
-          <div className="felx text-body1_m text-subtext1 space-y-[14px]">
-            <div className="flex flex-row gap-[65px]">
-              <p className="w-[56px]">모집 인원</p>
-              <p>6명</p>
+          <div className="h-0.5 bg-menuborder sm_md:w-[350px] mt-6 mb-6 md:hidden" />
+          <div className="flex flex-col md:pt-[6px]">
+            <div className="block md:hidden">
+              <div className="flex flex-col">
+                <div className="flex justify-between items-center">
+                  <div className="flex gap-3 items-center">
+                    <Image
+                      src={test_image}
+                      alt={"Test Image"}
+                      width={68}
+                      height={68}
+                      className="rounded-full object-cover"
+                    />
+                    <ClubProfileCard
+                      clubName={"동아리 이름"}
+                      affiliation={"소속"}
+                      field={"분야"}
+                      region={"지역"}
+                      target={"대상"}
+                    />
+                  </div>
+                  <div className="cursor-pointer" onClick={onHeartClick}>
+                    {isHeart ? (
+                      <MdFavorite size={24} color="#D1F75D" />
+                    ) : (
+                      <MdFavorite size={24} color="#E3E3E3" />
+                    )}
+                  </div>
+                </div>
+                <div className="h-0.5 bg-menuborder mt-6 mb-6" />
+                <RecruitmentSummary
+                  members={6}
+                  startDate={"2024.10.07"}
+                  endDate={"2024.11.02"}
+                  email={"123Sysys@gamil.com"}
+                />
+              </div>
             </div>
-            <div className="flex flex-row gap-[65px]">
-              <p className="w-[56px]">모집 기간</p>
-              <p>24.10.09 ~ 10.24</p>
+            {/* PC 화면 */}
+            <div className="hidden md:block">
+              <div className="flex items-center md:gap-5">
+                <div className="relative w-[60px] h-[60px]">
+                  <Image
+                    src={test_image}
+                    alt={"Test Image"}
+                    width={60}
+                    height={60}
+                    className="rounded-full object-cover"
+                  />
+                  <div
+                    className="absolute bottom-0 right-0 cursor-pointer"
+                    onClick={onHeartClick}
+                  >
+                    {isHeart ? (
+                      <MdFavorite size={20} color="#D1F75D" />
+                    ) : (
+                      <MdFavorite size={20} color="#E3E3E3" />
+                    )}
+                  </div>
+                </div>
+                <h3 className="text-subtext1 text-h3">동아리 이름</h3>
+              </div>
+              <h1 className="text-h1_contents_title md:mt-8 md:mb-12">
+                모집공고 제목 혹은 <br />
+                동아리 한줄 소개 작성
+              </h1>
+              <div className="w-[340px] mt-10">
+                <Keyword />
+              </div>
+              <div className="h-0.5 bg-menuborder mt-8 mb-7" />
+              <RecruitmentSummary
+                members={6}
+                startDate={"2024.10.07"}
+                endDate={"2024.11.02"}
+                email={"123Sysys@gamil.com"}
+              />
             </div>
-            <div className="flex flex-row gap-[65px]">
-              <p className="w-[56px]">이메일</p>
-              <p>12345@gmail.com</p>
+            <div className="fixed bottom-0 left-0 right-0 md:static mt-10">
+              <div className="bg-background px-4 pt-2 pb-6 md:px-0 md:pt-0 md:pb-0">
+                <RecruitmentBottomBar />
+              </div>
+            </div>
+            <div className="h-5" />
+            <div className="hidden md:flex">
+              <IconBtn
+                type={"declaration"}
+                size={"large"}
+                title={"신고하기"}
+                onClick={() => {}}
+              />
             </div>
           </div>
         </div>
-        <RecruitmentBottomBar />
+        {/* <RecruitmentGuideFloatingBar deadline={new Date("2024-12-31T23:59:59")} /> */}
+        {/* <DayFloatingBar deadline={new Date("2024-12-31T23:59:59")} /> */}
+        {/* <PointStatusFloatingBar /> */}
+        {/* <MobilePointStatusFloatingBar /> */}
       </div>
-      {/* <RecruitmentGuideFloatingBar deadline={new Date("2024-12-31T23:59:59")} /> */}
-      {/* <DayFloatingBar deadline={new Date("2024-12-31T23:59:59")} /> */}
-      {/* <PointStatusFloatingBar /> */}
-      {/* <MobilePointStatusFloatingBar /> */}
     </div>
   );
 };
