a601fcda5ff67c444005b641e0496221c57f3067
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-09 17:34:38 +0900

    feat: Update reviewBadge.ts

--- app/data/reviewBadge.ts
+++ app/data/reviewBadge.ts
@@ -6,16 +6,61 @@ import relationship from "@/images/icon/reviewBadge/relationship.svg";
 import selfDevelopment from "@/images/icon/reviewBadge/selfDevelopment.svg";
 import skill from "@/images/icon/reviewBadge/skill.svg";
 
-export const REVIEW_BADGE_LIST = [
-  { id: 1, imageUrl: employment, title: "취업준비에 도움이 돼요" },
-  { id: 2, imageUrl: experience, title: "인간관계를 넓힐 수 있어요" },
-  { id: 3, imageUrl: health, title: "관심분야를 탐구할 수 있어요" },
-  { id: 4, imageUrl: interest, title: "자기 계발에 도움이 돼요" },
+export interface Badge {
+  id: number;
+  imageUrl: string;
+  title: string;
+  type:
+    | "employment"
+    | "experience"
+    | "health"
+    | "interest"
+    | "relationship"
+    | "selfDevelopment"
+    | "skill";
+}
+
+export const REVIEW_BADGE_LIST: Badge[] = [
+  {
+    id: 1,
+    imageUrl: employment,
+    title: "취업준비에 도움이 돼요",
+    type: "employment",
+  },
+  {
+    id: 2,
+    imageUrl: experience,
+    title: "인간관계를 넓힐 수 있어요",
+    type: "experience",
+  },
+  {
+    id: 3,
+    imageUrl: health,
+    title: "관심분야를 탐구할 수 있어요",
+    type: "health",
+  },
+  {
+    id: 4,
+    imageUrl: interest,
+    title: "자기 계발에 도움이 돼요",
+    type: "interest",
+  },
   {
     id: 5,
     imageUrl: relationship,
     title: "전공 실력이나 성적을 높일 수 있어요",
+    type: "relationship",
   },
-  { id: 6, imageUrl: selfDevelopment, title: "건강증진에 도움이 돼요" },
-  { id: 7, imageUrl: skill, title: "다양한 경험을 할 수 있어요" },
+  {
+    id: 6,
+    imageUrl: selfDevelopment,
+    title: "건강증진에 도움이 돼요",
+    type: "selfDevelopment",
+  },
+  {
+    id: 7,
+    imageUrl: skill,
+    title: "다양한 경험을 할 수 있어요",
+    type: "skill",
+  },
 ];
