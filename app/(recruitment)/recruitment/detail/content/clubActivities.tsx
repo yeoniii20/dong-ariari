e29ed236772548ae5c22ad6fa6459d6c39a56430
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-09 15:34:51 +0900

    fix: edit mock data

--- app/(recruitment)/recruitment/detail/content/clubActivities.tsx
+++ app/(recruitment)/recruitment/detail/content/clubActivities.tsx
@@ -28,16 +28,19 @@ const RECRUITMENT_CARDS = [
     id: 0,
     title: "2024년 봄학기 동아리 모집",
     date: "2024.12.31",
+    status: "모집중",
   },
   {
     id: 1,
     title: "가을학기 동아리 모집 공고",
     date: "2024.10.15",
+    status: "모집중",
   },
   {
     id: 2,
     title: "여름방학 동아리 모집 안내",
     date: "2024.07.20",
+    status: "모집마감",
   },
 ];
 
@@ -78,6 +81,7 @@ const ClubActivities = () => {
                 title={item.title}
                 date={item.date}
                 onClick={() => {}}
+                status={item.status === "모집중" ? "enable" : "disable"}
               />
             );
           })}
