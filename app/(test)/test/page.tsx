ccd8b2791658bfc9fa0dde991e48708a71e8b083
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-09 16:20:22 +0900

    feat: Update test page.tsx

--- app/(test)/test/page.tsx
+++ app/(test)/test/page.tsx
@@ -3,24 +3,40 @@
 import { useState } from "react";
 import MobileButtons from "../components/buttons/mobile_Buttons";
 import PcButtons from "../components/buttons/PC_Buttons";
+import Badges from "../components/badges";
 
 const TestPage = () => {
-  const [isButtonVisible, setIsButtonVisible] = useState(true);
+  const [isButtonVisible, setIsButtonVisible] = useState<boolean>(false);
+  const [isBadgeVisible, setIsBadgeVisible] = useState<boolean>(true);
 
   return (
     <div>
       <button
-        className="py-2 px-4 mb-3 bg-pink-400 text-white rounded-lg hover:bg-pink-500"
+        className="py-2 px-4 mb-3 bg-pink-400 text-white text-body3_r
+        rounded-lg hover:bg-pink-500"
         onClick={() => setIsButtonVisible(!isButtonVisible)}
       >
         {isButtonVisible
           ? "Hidden Button Components"
           : "Show Button Components"}
       </button>
+      <button
+        className="py-2 px-4 mb-3 bg-yellow-400 text-white text-body3_r
+        rounded-lg hover:bg-yellow-500"
+        onClick={() => setIsBadgeVisible(!isBadgeVisible)}
+      >
+        {isBadgeVisible ? "Hidden Badge Components" : "Show Badge Components"}
+      </button>
+
       {isButtonVisible && (
         <div className="bg-pink-100 p-3">
           <PcButtons />
           <MobileButtons />
+        </div>
+      )}
+      {isBadgeVisible && (
+        <div className="bg-gray-100 p-3">
+          <Badges />
         </div>
       )}
     </div>
