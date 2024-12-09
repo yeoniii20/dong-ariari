6fea13d5f386c1a2ba970af96d075275bd13ea08
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-09 15:34:21 +0900

    feat: Update recruitmentCard.tsx

--- app/components/card/recruitmentCard.tsx
+++ app/components/card/recruitmentCard.tsx
@@ -6,9 +6,15 @@ interface RecruitmentCardProps {
   onClick: () => void;
   title: string;
   date: string;
+  status: "enable" | "disable";
 }
 
-const RecruitmentCard = ({ onClick, title, date }: RecruitmentCardProps) => {
+const RecruitmentCard = ({
+  onClick,
+  title,
+  date,
+  status,
+}: RecruitmentCardProps) => {
   return (
     <div
       className="w-full m-w-[1248px] p-4 md:py-[26px] md:pl-9 md:pr-[30px] rounded-lg
@@ -18,7 +24,7 @@ const RecruitmentCard = ({ onClick, title, date }: RecruitmentCardProps) => {
       <div className="flex flex-col items-start md:flex-row md:justify-between md:items-center">
         <div className="flex flex-col items-start md:flex-row  md:items-center gap-3 md:gap-12">
           <div className="flex gap-2">
-            <Badge text={"모집중"} textColor={"blue"} />
+            <Badge status={status} />
           </div>
           <h3 className="text-body1_m md:text-h4_sb text-text1">{title}</h3>
         </div>
