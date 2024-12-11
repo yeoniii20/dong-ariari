959d74eb7c369a975183815e72b247d17da2443c
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-11 10:25:28 +0900

    feat: Add JsDocs and apply tailwind design scale

--- app/components/pulldown/singleSelectOptions.tsx
+++ app/components/pulldown/singleSelectOptions.tsx
@@ -7,6 +7,14 @@ interface SingleSelectOptionsProps {
   handleMenuClick: (label: string) => void;
 }
 
+/**
+ *
+ * @param selectedOption 선택한 옵션
+ * @param optionData 옵션 데이터
+ * @param size 사이즈
+ * @param handleMenuClick 메뉴 클릭 핸들러
+ * @returns
+ */
 const SingleSelectOptions = ({
   selectedOption,
   optionData,
@@ -14,32 +22,34 @@ const SingleSelectOptions = ({
   handleMenuClick,
 }: SingleSelectOptionsProps) => (
   <div
-    className={`z-50 absolute top-full left-1/2 transform -translate-x-1/2 mt-2 bg-background rounded-lg border border-menuborder shadow-default ${
-      size == "small"
-        ? "w-[116px]"
-        : size == "medium"
-        ? "w-[160px]"
-        : "w-[190px]"
-    } `}
+    className={`z-50 absolute top-full left-1/2 transform -translate-x-1/2 mt-2 bg-background
+      rounded-lg border border-menuborder shadow-default ${
+        size == "small"
+          ? "w-[116px]"
+          : size == "medium"
+          ? "w-[160px]"
+          : "w-[190px]"
+      } `}
   >
     {optionData.map((item, index) => (
       <div
         key={item.id}
-        className={`relative flex justify-center items-center text-base text-subtext1 cursor-pointer  pressed:bg-pressed
+        className={`relative flex justify-center items-center text-base 
+          text-subtext1 cursor-pointer pressed:bg-pressed
             ${index === 0 ? "rounded-t-lg" : ""}
             ${index === optionData.length - 1 ? "rounded-b-lg" : ""}
             ${index !== 0 ? "border-t border-menuborder " : ""}
             ${
               selectedOption === item.label
-                ? `border border-primary bg-selectedoption_default mx-[0px]`
-                : `mx-[5px] hover:bg-hover hover:mx-[0px] hover:text-subtext1`
+                ? `border border-primary bg-selectedoption_default mx-[-1.5px]`
+                : `mx-[5px] hover:bg-hover hover:mx-0 hover:text-subtext1`
             }          
             `}
         onMouseDown={(e) => e.preventDefault()}
         onClick={() => handleMenuClick(item.label)}
       >
         <span
-          className={`relative w-full py-[10px] text-center text-[15px] ${
+          className={`relative w-full py-2.5 text-center text-[15px] ${
             selectedOption === item.label && "text-primary"
           }`}
         >
