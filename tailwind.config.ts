b7886a74fad485aceaa034225c691927667ac1d7
suyoooi <100350818+yeoniii20@users.noreply.github.com>
2024-12-24 10:34:58 +0900

    feat: Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -70,7 +70,6 @@ const config: Config = {
         28: "28px",
         19: "19px",
         15: "15px",
-        14: "14px",
         13: "13px",
         10: "10px",
         h1_contents_title: ["24px", { fontWeight: 600 }],
